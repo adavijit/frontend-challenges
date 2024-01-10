5f25dc082482dae1e4d3f2de9d5d67224d88c751
Avijit Das <adavijit@yahoo.com>
2024-01-10 19:37:26 +0530

    Updating Readmeee.md file

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,18 @@
 import ChessBoard from "./Challenges/ChessBoard/ChessBoard";
 import TicTacToe from "./Challenges/TicTacToe/TicTacToe";
 
+const basePath = "https://github.com/adavijit/frontend-challenges/tree/main/src/Challenges";
 const challenges = [
     {
         "name": "Tic-Tac-Toe",
         "contributors": ["adavijit"],
-        "source": "www",
+        "source": `${basePath}/TicTacToe`,
         "component": <TicTacToe />
     },
     {
         "name": "Chess Board",
         "contributors": ["adavijit"],
-        "source": "www",
+        "source": `${basePath}/ChessBoard`,
         "component": <ChessBoard />
     },
 ];
