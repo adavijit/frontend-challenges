69ddaa8264b4dd573eabf7efa5f333220345655b
Avijit Das <adavijit@yahoo.com>
2024-01-11 11:41:22 +0530

    Updated UI

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,30 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Card.css";
 
 const Card = ({ name, onClick, component, contributors, source }) => {
+  // predefined list of colors
+  const colors = ["#EAC5D8", "#d4e1ca", "#c0e6de", "#dee6c0", "#c7d2e2"];
 
-      // predefined list of colors
-      const colors = ['#EAC5D8', '#d4e1ca', '#c0e6de', '#dee6c0', '#c7d2e2'];
+  // Function to get a random color from the predefined list
+  function getRandomColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
 
-        
-      // Function to get a random color from the predefined list
-      function getRandomColor() {
-        const randomIndex = Math.floor(Math.random() * colors.length);
-        return colors[randomIndex];
-    }
-
-  
   return (
-    <div className="card"  style={{ backgroundColor: getRandomColor() }}>
+    <div className="card" style={{ backgroundColor: getRandomColor() }}>
       <div className="text">
         <p className="card-heading">{name}</p>
         <p className="card-subheading">Contributed by</p>
         <p className="card-subheading">
-          {contributors.map((contributor)=> {
-            return <a href={`https://www.github.com/${contributor}`} target="_blank" rel="noreferrer" >{contributor}</a>
-        })}</p>
+          {contributors.map((contributor) => {
+            return (
+              <a
+                href={`https://www.github.com/${contributor}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {contributor}
+              </a>
+            );
+          })}
+        </p>
       </div>
-      <div className="arrow-btn" onClick={() => onClick(component, source, name)}></div>
+      <div
+        className="arrow-btn"
+        onClick={() => onClick(component, source, name)}
+      ></div>
     </div>
   );
 };
